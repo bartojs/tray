fd656949da1313021863b6ff662af57381e7a2b6
justin barton <bartonjustin@gmail.com>
2013-08-06 21:08:28 +1200

    add exit. dont need trayicon listeners

--- src/tray/core.clj
+++ src/tray/core.clj
@@ -1,25 +1,17 @@
 (ns tray.core
   (:require [clojure.java.io :as io])
-  (:import
-	[javax.swing UIManager SwingUtilities JOptionPane ImageIcon]
+  (:import [javax.swing UIManager SwingUtilities JOptionPane ImageIcon]
     [java.awt SystemTray TrayIcon PopupMenu Menu MenuItem]
-    [java.awt.event ActionListener]
+    [java.awt.event ActionListener ItemListener]
     [java.net URL]))
 
-(defn show []
- (let [pop (PopupMenu.)
-       icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif")) "icon")))
-       tray (SystemTray/getSystemTray)
-       ;;menu (Menu.)
-       item (MenuItem. "About")]
-    (.add pop item)
-    (.setPopupMenu icon pop)
-    (.add tray icon)
-    (.addActionListener icon 
-      (reify ActionListener 
-         (actionPerformed [this _] (JOptionPane/showMessageDialog nil "Hello Tray"))))))
-
 (defn -main [& args] 
-    ;;(UIManager/setLookAndFeel "com.sun.java.swing.plaf.windows.WindowsLookAndFeel")
-    ;;(UIManager/put "swing.boldMetal" false)
-    (SwingUtilities/invokeLater show))
+  (SwingUtilities/invokeLater 
+    (fn []
+      (let [icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif")) "icon")))
+            item (doto (MenuItem. "Exit") 
+                   (.addActionListener 
+                    (reify ActionListener (actionPerformed [this _] (.remove (SystemTray/getSystemTray) icon) (System/exit 0)))))
+            popup (doto (PopupMenu.) (.add item))]
+        (.setPopupMenu icon popup)
+        (.add (SystemTray/getSystemTray) icon)))))
