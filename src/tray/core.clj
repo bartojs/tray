0bfbbca5f9fd0f344808a7c1bf936bfedaaf80f9
justin barton <bartonjustin@gmail.com>
2013-08-06 23:18:53 +1200

    add and remove items

--- src/tray/core.clj
+++ src/tray/core.clj
@@ -2,21 +2,23 @@
   (:require [clojure.java.io :as io])
   (:import [javax.swing UIManager SwingUtilities ImageIcon]
     [java.awt SystemTray TrayIcon PopupMenu Menu MenuItem CheckboxMenuItem]
-    [java.awt.event ActionListener ItemListener]))
+    [java.awt.event ActionListener ItemListener ItemEvent]))
 
 (def current-item (atom nil))
-(def items (atom ["tech support" "kayako" "retail"]))
-(def checkboxes (atom []))
+(def checkboxes (atom {"tech support" nil "kayako" nil "retail" nil}))
 (defn add-checkbox [item]
    (let [cb (doto (CheckboxMenuItem. item) 
              (.addItemListener (reify ItemListener 
                                  (itemStateChanged [this event]
                                    (println "statechange" (.getLabel (.getItemSelectable event)))
-                                  (doseq [cx @checkboxes] 
+                                  (if (= (.getStateChange event) ItemEvent/SELECTED)
+                                    (swap! current-item (constantly  (.getItemSelectable event))) 
+                                    (.setState (.getItemSelectable event) true))
+                                  (doseq [cx (vals @checkboxes)] 
                                     (println "off" (.getLabel cx))
                                     (when-not (identical? (.getItemSelectable event) cx) 
                                                             (.setState cx false)))))))]
-     (swap! checkboxes conj cb)
+     (swap! checkboxes conj [item cb])
      cb))
 
 (defn -main [& args] 
@@ -24,10 +26,18 @@
     (fn []
       (let [tray (SystemTray/getSystemTray)
             icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif")) "icon")))
+            popup (PopupMenu.)
             exititem (doto (MenuItem. "Exit") 
                    (.addActionListener 
                     (reify ActionListener (actionPerformed [this _] (.remove tray icon) (System/exit 0)))))
-            popup (doto (PopupMenu.) (.add exititem) (.addSeparator))]
-        (doseq [item @items] (.add popup (add-checkbox item))) 
+            additem (doto (MenuItem. "Add") 
+                   (.addActionListener 
+                    (reify ActionListener (actionPerformed [this _] (.add popup (add-checkbox "new"))))))
+            removeitem (doto (MenuItem. "Remove") 
+                   (.addActionListener 
+                    (reify ActionListener (actionPerformed [this _] (when @current-item (.remove popup @current-item) 
+                                                                      (swap! checkboxes dissoc (.getLabel @current-item)))))))
+            popup (doto popup (.add additem) (.add removeitem) (.add exititem) (.addSeparator))]
+        (doseq [item (keys @checkboxes)] (.add popup (add-checkbox item))) 
         (.setPopupMenu icon popup)
         (.add tray icon)))))
