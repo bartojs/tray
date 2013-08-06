a9769f8ea547237534db72626a752a7f7bbd5ec4
justin barton <bartonjustin@gmail.com>
2013-08-07 00:20:54 +1200

    edit working

--- src/tray/core.clj
+++ src/tray/core.clj
@@ -1,6 +1,6 @@
 (ns tray.core
   (:require [clojure.java.io :as io])
-  (:import [javax.swing UIManager SwingUtilities ImageIcon]
+  (:import [javax.swing UIManager SwingUtilities ImageIcon JOptionPane]
     [java.awt SystemTray TrayIcon PopupMenu Menu MenuItem CheckboxMenuItem]
     [java.awt.event ActionListener ItemListener ItemEvent]))
 
@@ -10,16 +10,28 @@
    (let [cb (doto (CheckboxMenuItem. item) 
              (.addItemListener (reify ItemListener 
                                  (itemStateChanged [this event]
-                                   (println "statechange" (.getLabel (.getItemSelectable event)))
+                                  (let [selected (.getItemSelectable event)]
                                   (if (= (.getStateChange event) ItemEvent/SELECTED)
-                                    (swap! current-item (constantly  (.getItemSelectable event))) 
-                                    (.setState (.getItemSelectable event) true))
+                                    (do (swap! current-item (constantly (.getItem event)))
+                                      (println "change" (java.util.Date.) (.getLabel selected))) 
+                                    (.setState selected true))
                                   (doseq [cx (vals @checkboxes)] 
-                                    (println "off" (.getLabel cx))
-                                    (when-not (identical? (.getItemSelectable event) cx) 
-                                                            (.setState cx false)))))))]
+                                    (when-not (identical? selected cx) 
+                                                            (.setState cx false))))))))]
      (swap! checkboxes conj [item cb])
      cb))
+
+(defn remove-checkbox [item]
+   (swap! checkboxes dissoc item))
+
+(defn update-checkbox [item]
+  (when item
+   (let [cb (get @checkboxes @current-item)] 
+    (println "edit" (java.util.Date.) item) 
+    (.setLabel cb item)
+    (swap! checkboxes dissoc @current-item)
+    (swap! checkboxes assoc item cb)
+    (swap! current-item (constantly item)))))
 
 (defn -main [& args] 
   (SwingUtilities/invokeLater 
@@ -33,11 +45,17 @@
             additem (doto (MenuItem. "Add") 
                    (.addActionListener 
                     (reify ActionListener (actionPerformed [this _] (.add popup (add-checkbox "new"))))))
+            edititem (doto (MenuItem. "Edit") 
+                   (.addActionListener 
+                    (reify ActionListener (actionPerformed [this _]
+                       (when @current-item
+                       (update-checkbox (JOptionPane/showInputDialog nil "Edit" "Edit" JOptionPane/PLAIN_MESSAGE nil nil @current-item))))))) 
             removeitem (doto (MenuItem. "Remove") 
                    (.addActionListener 
-                    (reify ActionListener (actionPerformed [this _] (when @current-item (.remove popup @current-item) 
-                                                                      (swap! checkboxes dissoc (.getLabel @current-item)))))))
-            popup (doto popup (.add additem) (.add removeitem) (.add exititem) (.addSeparator))]
+                    (reify ActionListener (actionPerformed [this _] (when @current-item 
+                                                                      (.remove popup (get @checkboxes @current-item)) 
+                                                                      (remove-checkbox @current-item))))))
+            popup (doto popup (.add additem) (.add edititem) (.add removeitem) (.add exititem) (.addSeparator))]
         (doseq [item (keys @checkboxes)] (.add popup (add-checkbox item))) 
         (.setPopupMenu icon popup)
         (.add tray icon)))))
