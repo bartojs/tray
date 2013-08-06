05fa315bae5244628aaacd864884efbc4207bc15
justin barton <bartonjustin@gmail.com>
2013-08-06 22:34:05 +1200

    add checkboxes/radioboxes

--- src/tray/core.clj
+++ src/tray/core.clj
@@ -1,17 +1,33 @@
 (ns tray.core
   (:require [clojure.java.io :as io])
-  (:import [javax.swing UIManager SwingUtilities JOptionPane ImageIcon]
-    [java.awt SystemTray TrayIcon PopupMenu Menu MenuItem]
-    [java.awt.event ActionListener ItemListener]
-    [java.net URL]))
+  (:import [javax.swing UIManager SwingUtilities ImageIcon]
+    [java.awt SystemTray TrayIcon PopupMenu Menu MenuItem CheckboxMenuItem]
+    [java.awt.event ActionListener ItemListener]))
+
+(def current-item (atom nil))
+(def items (atom ["tech support" "kayako" "retail"]))
+(def checkboxes (atom []))
+(defn add-checkbox [item]
+   (let [cb (doto (CheckboxMenuItem. item) 
+             (.addItemListener (reify ItemListener 
+                                 (itemStateChanged [this event]
+                                   (println "statechange" (.getLabel (.getItemSelectable event)))
+                                  (doseq [cx @checkboxes] 
+                                    (println "off" (.getLabel cx))
+                                    (when-not (identical? (.getItemSelectable event) cx) 
+                                                            (.setState cx false)))))))]
+     (swap! checkboxes conj cb)
+     cb))
 
 (defn -main [& args] 
   (SwingUtilities/invokeLater 
     (fn []
-      (let [icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif")) "icon")))
-            item (doto (MenuItem. "Exit") 
+      (let [tray (SystemTray/getSystemTray)
+            icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif")) "icon")))
+            exititem (doto (MenuItem. "Exit") 
                    (.addActionListener 
-                    (reify ActionListener (actionPerformed [this _] (.remove (SystemTray/getSystemTray) icon) (System/exit 0)))))
-            popup (doto (PopupMenu.) (.add item))]
+                    (reify ActionListener (actionPerformed [this _] (.remove tray icon) (System/exit 0)))))
+            popup (doto (PopupMenu.) (.add exititem) (.addSeparator))]
+        (doseq [item @items] (.add popup (add-checkbox item))) 
         (.setPopupMenu icon popup)
-        (.add (SystemTray/getSystemTray) icon)))))
+        (.add tray icon)))))
