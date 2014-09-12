fb743a82bd7dcfa38a092a3ea4c3a9777d5689dd
justin barton <bartonjustin@gmail.com>
2014-09-12 12:13:39 +1200

    try drag and drop

---
+++ src/tray/awt.clj
@@ -0,0 +1,29 @@
+(ns awt (:import [java.awt Frame Label SystemTray TrayIcon PopupMenu MenuItem] 
+                 [java.awt.event WindowAdapter WindowEvent ActionListener] 
+                 [java.awt.dnd DropTarget DropTargetListener DnDConstants]
+                 [java.awt.datatransfer DataFlavor]
+                 [javax.swing ImageIcon])
+        (:require [clojure.java.io :as io]))
+
+(def label (Label. "Hello world"))
+(def frame (doto (Frame. "Hello") (.add label) (.setSize 300 100) (.addWindowListener (proxy [WindowAdapter] [] (windowClosing [event] (.setVisible (.getWindow event) false))))))
+(def tray (SystemTray/getSystemTray))
+(def icon (TrayIcon. (.getImage (ImageIcon. (io/as-url (io/resource "bulb.gif"))))))
+
+(def dnd (DropTarget. frame (reify DropTargetListener 
+                              (dragEnter [this _])
+                              (dragExit [this _])
+                              (dragOver [this _])
+                              (dropActionChanged [this _])
+                              (drop [this event] 
+                                (.acceptDrop event DnDConstants/ACTION_LINK)
+	                        (doto frame (.removeAll) 
+                                            (.add (Label. (str "dropped " (.. event (getTransferable) (getTransferData DataFlavor/stringFlavor))))) 
+                                            (.repaint))))))
+
+(def popup (doto (PopupMenu.) 
+             (.add (doto (MenuItem. "Open") (.addActionListener (reify ActionListener (actionPerformed [this _ ] (.setVisible frame true))))))
+             (.add (doto (MenuItem. "Exit") (.addActionListener (reify ActionListener (actionPerformed [this _ ] (.remove tray icon) (System/exit 0)))))))) 
+
+(.setPopupMenu icon popup)
+(.add tray icon)
